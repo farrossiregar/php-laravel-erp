5d8b3a4c827793309edb86520c04b464d45a2062
Ramdoni <doni.engine@gmail.com>
2021-01-25 21:05:55 +0700

    Update sidebar

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::post('login', [\App\Http\Controllers\Api\UserController::class,'login']);
+Route::post('register', 'Api\UserController@register');
+
+Route::group(['middleware' => 'auth:api'], function(){
+	Route::post('details', 'Api\UserController@details');
+});
+
+Route::post('login',function(){
+    
 });
