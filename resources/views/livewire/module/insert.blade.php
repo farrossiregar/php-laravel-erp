7dd98be414af9ff9e4554d08e0359371f4efe5ca
Ramdoni <doni.engine@gmail.com>
2021-02-05 10:27:04 +0700

    Update module icon

--- resources/views/livewire/module/insert.blade.php
+++ resources/views/livewire/module/insert.blade.php
@@ -20,18 +20,11 @@
                         <ul class="parsley-errors-list filled" id="parsley-id-29"><li class="parsley-required">{{ $message }}</li></ul>
                         @enderror
                     </div>
-                    <div class="form-group">
-                        <label>{{ __('Prefix Link') }}</label>
-                        <input type="text" class="form-control" wire:model="prefix_link" >
-                        @error('prefix_link')
-                        <ul class="parsley-errors-list filled" id="parsley-id-29"><li class="parsley-required">{{ $message }}</li></ul>
-                        @enderror
-                    </div>
                     <div class="row">
                         <div class="form-group col-md-6">
                             <label>{{ __('icon') }}</label>
-                            <input type="text" class="form-control" wire:model="prefix_link" >
-                            @error('prefix_link')
+                            <input type="text" class="form-control" wire:model="icon" >
+                            @error('icon')
                             <ul class="parsley-errors-list filled" id="parsley-id-29"><li class="parsley-required">{{ $message }}</li></ul>
                             @enderror
                         </div>
