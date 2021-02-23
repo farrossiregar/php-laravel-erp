776ff192893af5d9cd05a3d8bf587bf8ba7b5dac
Ramdoni <doni.engine@gmail.com>
2021-02-23 11:50:31 +0700

    Update api CAC

--- app/Http/Controllers/Api/CustomerAssetManagementController.php
+++ app/Http/Controllers/Api/CustomerAssetManagementController.php
@@ -12,8 +12,22 @@ class CustomerAssetManagementController extends Controller
 {
     public function data(Request $r){
 
-        $data = \App\Models\CustomerAssetManagement::paginate(10);
-
+        $raw = \App\Models\CustomerAssetManagement::paginate(10);
+        $data = [];
+        foreach($raw as $k => $item)
+        {
+            $var = [];
+            if(!isset($item->tower->name) or $item->tower->name=="") continue;
+            $var['id'] = $item->id;
+            $var['uploader'] = date('d F Y',strtotime($item->created_at));
+            $var['tower'] = $item->tower->name;
+            $var['site_id'] = isset($item->site->site_id) ? $item->site->site_id : '';
+            $var['site'] = isset($item->site->name) ? $item->site->name : '';
+            $var['region'] = isset($item->region->region) ? $item->region->region : '';
+            $data[] = $var;
+        }
+        $json['current_page'] = 1;
+        $json['data'] = $data;
         return response()->json(['status'=>200,'data'=>$data], 200);
     }
 }
