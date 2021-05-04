7ca64d95ef4a7e8c9d17420d88570e29d1ea8b69
farrossiregar <farros.jackson@gmail.com>
2021-05-04 21:46:38 +0700

    update status po non ms, hidden button bast upload

--- resources/views/livewire/po-tracking-nonms/edit-bast.blade.php
+++ resources/views/livewire/po-tracking-nonms/edit-bast.blade.php
@@ -40,10 +40,10 @@
                                         @if(check_access('po-tracking-nonms.import-bast'))
                                             @if($item->bast == null || $item->bast == '')
                                                 @if($item->po_no != '' || $item->po_no != null)
-                                                    <a href="javascript:;" wire:click="$emit('modalimportbast','{{$item->id}}')"  data-toggle="modal" data-target="#modal-potrackingnonms-importbast" title="Upload" class="btn btn-primary"><i class="fa fa-plus"></i> {{__('Import Bast')}}</a>
+                                                    <!-- <a href="javascript:;" wire:click="$emit('modalimportbast','{{$item->id}}')"  data-toggle="modal" data-target="#modal-potrackingnonms-importbast" title="Upload" class="btn btn-primary"><i class="fa fa-plus"></i> {{__('Import Bast')}}</a> -->
                                                 @endif
                                             @else
-                                                <a href="javascript:;" wire:click="$emit('modalimportbast','{{$item->id}}')"  data-toggle="modal" data-target="#modal-potrackingnonms-importbast" title="Upload" class="btn btn-primary"><i class="fa fa-edit"></i> </a>
+                                                <!-- <a href="javascript:;" wire:click="$emit('modalimportbast','{{$item->id}}')"  data-toggle="modal" data-target="#modal-potrackingnonms-importbast" title="Upload" class="btn btn-primary"><i class="fa fa-edit"></i> </a> -->
 
                                                 <a href="<?php echo asset('storage/po_tracking_nonms/Bast/'.$item->bast) ?>" target="_blank"><i class="fa fa-download"></i> Download Bast </a>
                                             @endif
