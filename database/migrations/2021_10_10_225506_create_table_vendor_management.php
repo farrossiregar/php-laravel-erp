059354110a3f08635bab8be0834515f8ed90918d
Ramdoni <doni.engine@gmail.com>
2021-10-12 17:26:12 +0700

    update: uncomment migration

--- database/migrations/2021_10_10_225506_create_table_vendor_management.php
+++ database/migrations/2021_10_10_225506_create_table_vendor_management.php
@@ -13,26 +13,26 @@ class CreateTableVendorManagement extends Migration
      */
     public function up()
     {
-        // Schema::create('vendor_management', function (Blueprint $table) {
-        //     $table->id();
-        //     $table->char('supplier_name', 30)->nullable();
-        //     $table->char('supplier_pic', 30)->nullable();
-        //     $table->char('supplier_category', 30)->nullable();
-        //     $table->char('supplier_registration_date', 30)->nullable();
-        //     $table->char('status', 30)->nullable();
-        //     $table->char('note', 30)->nullable();
-        //     $table->char('legal', 100)->nullable();
-        //     $table->char('org_chart', 100)->nullable();
-        //     $table->char('tools_resource', 100)->nullable();
-        //     $table->char('certification_resource', 100)->nullable();
-        //     $table->char('scoring', 100)->nullable();
-        //     $table->timestamps();
-        // });
+        Schema::create('vendor_management', function (Blueprint $table) {
+            $table->id();
+            $table->char('supplier_name', 30)->nullable();
+            $table->char('supplier_pic', 30)->nullable();
+            $table->char('supplier_category', 30)->nullable();
+            $table->char('supplier_registration_date', 30)->nullable();
+            $table->char('status', 30)->nullable();
+            $table->char('note', 30)->nullable();
+            $table->char('legal', 100)->nullable();
+            $table->char('org_chart', 100)->nullable();
+            $table->char('tools_resource', 100)->nullable();
+            $table->char('certification_resource', 100)->nullable();
+            $table->char('scoring', 100)->nullable();
+            $table->timestamps();
+        });
 
-        // Schema::create('supplier_selection', function (Blueprint $table) {
-        //     $table->id();
-        //     $table->timestamps();
-        // });
+        Schema::create('supplier_selection', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
     }
 
     /**
