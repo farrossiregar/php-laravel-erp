23d146a5cbbe412c6555973cfbe5e2dfc960cd3c
Ramdoni <doni.engine@gmail.com>
2021-12-07 06:14:23 +0700

    update:main kpi

--- app/Http/Livewire/WorkFlowManagement/Index.php
+++ app/Http/Livewire/WorkFlowManagement/Index.php
@@ -3,51 +3,16 @@
 namespace App\Http\Livewire\WorkFlowManagement;
 
 use Livewire\Component;
-use Livewire\WithPagination;
 class Index extends Component
 {
-    use WithPagination;
-    protected $paginationTheme = 'bootstrap';
-    public $layout_chart_parent,$layout_chart_parent_id,$layout_chart,$layout_chart_init_refresh;
-    public $year,$region,$month;
-    protected $listeners = ['refresh-page'=>'$refresh'];
+    
     public function render()
     {
         if(!check_access('work-flow-management.index')){
             session()->flash('message-error','Access denied, you have no permission please contact your administrator.');
             $this->redirect('/');
         }
-        $data = \App\Models\WorkFlowManagement::orderBy('updated_at','DESC');
-        return view('livewire.work-flow-management.index')->with(['data'=>$data->paginate(100)]);
+        return view('livewire.work-flow-management.index');
     }
-    public function updated($propertyName)
-    {
-        if($propertyName == 'year') $this->emit('emit-year',$this->year);
-        if($propertyName == 'month') $this->emit('emit-month',$this->month);
-        if($propertyName == 'region') $this->emit('emit-region',$this->region);
-    }
-    public function mount()
-    {
-        $this->layout_chart_parent_id = 1;
-        $this->layout_chart_parent = 'total-ft-never-close-manual';
-        $this->layout_chart =[2=>'assigned-never-accept-wo',3=>'accept-never-close-wo-manual',4=>'wo-never-assigned'];
-        $this->layout_chart_init_refresh =['wo-never-assigned'=>'chart','assigned-never-accept-wo'=>'init-chart-assigned-never-accept-wo','accept-never-close-wo-manual'=>'init-chart-accept-never-close-wo-manual','total-ft-never-close-manual'=>'init-chart-total-ft-never-close-manual'];
-    }
-    public function set_layout($view,$id)
-    {
-        $temp = [];
-        $parent_before = $this->layout_chart_parent;
-        foreach($this->layout_chart as $id => $v){
-            if($view == $v)
-                $temp[date('his')] = $this->layout_chart_parent;
-            else
-                $temp[$id] = $v;
-        }
-        $this->layout_chart = $temp;
-        $this->layout_chart_parent_id = date('his')+1;
-        $this->layout_chart_parent = $view;
-
-        $this->emit($this->layout_chart_init_refresh[$parent_before]);
-        $this->emit($this->layout_chart_init_refresh[$this->layout_chart_parent]);
-    }
+    
 }
--- resources/views/livewire/work-flow-management/index.blade.php
+++ resources/views/livewire/work-flow-management/index.blade.php
@@ -17,38 +17,7 @@
             <div class="tab-content">
                 @if(check_access('work-flow-management.dashboard'))
                 <div class="tab-pane show active" id="wo-never-assigned">
-                    <div class="header row">
-                        <div class="col-md-1">
-                            <select class="form-control" wire:model="year">
-                                @foreach(\App\Models\WorkFlowManagement::select(\DB::raw('YEAR(date) as tahun'))->groupBy('tahun')->get() as $item)
-                                <option>{{$item->tahun}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="col-md-2" wire:ignore>
-                            <select class="multiselect multiselect-custom multiselect_region" style="width:100%;" wire:model="region" multiple="multiple">
-                                @foreach(\App\Models\WorkFlowManagement::groupBy('region')->get() as $item)
-                                @if(empty($item->region))@continue @endif
-                                <option>{{$item->region}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="col-md-2" wire:ignore>
-                            <select class="multiselect multiselect-custom multiselect_month" style="width:100%;" wire:model="month" multiple="multiple">
-                                @foreach(\App\Models\WorkFlowManagement::select(\DB::raw('MONTH(date) as month'))->groupBy('month')->get() as $item)
-                                @if(empty($item->month))@continue @endif
-                                <option value="{{$item->month}}">{{date('F', mktime(0, 0, 0, $item->month, 10))}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="col-md-7">
-                            <label wire:loading>
-                                <i class="fa fa-spinner fa-pulse fa-2x fa-fw"></i>
-                                <span class="sr-only">{{ __('Loading...') }}</span>
-                            </label>
-                        </div>
-                    </div>
-                    @livewire("work-flow-management.{$layout_chart_parent}", key($layout_chart_parent_id))
+                    @livewire('work-flow-management.dashboard')
                 </div>
                 @endif
                 {{--@if(check_access('work-flow-management.internal-review'))
