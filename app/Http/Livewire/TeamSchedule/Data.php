157c7f2aba75dcb6b4b275a0f72aef2f6c35f4b1
Ramdoni <doni.engine@gmail.com>
2021-12-19 17:59:43 +0700

    update: clear filter

--- app/Http/Livewire/TeamSchedule/Data.php
+++ app/Http/Livewire/TeamSchedule/Data.php
@@ -19,24 +19,13 @@ class Data extends Component
     
     public function render()
     {
-
-       
         $data = \App\Models\TeamScheduleNoc::where('company_name', Session::get('company_id'))->orderBy('created_at', 'desc');
-            //    dd($data->get());
-
-        // if($this->date) $ata = $data->whereDate('created_at',$this->date);
-        if($this->filteryear) $ata = $data->whereYear('start_schedule',$this->filteryear);
-        if($this->filtermonth) $ata = $data->whereMonth('start_schedule',$this->filtermonth);                
-        if($this->filterweek) $ata = $data->where('week',$this->filterweek);                        
-        if($this->filterproject) $ata = $data->where('project',$this->filterproject);                        
         
-        return view('livewire.team-schedule.data')->with(['data'=>$data->paginate(50)]);
-
+        if($this->filteryear) $data->whereYear('start_schedule',$this->filteryear);
+        if($this->filtermonth) $data->whereMonth('start_schedule',$this->filtermonth);                
+        if($this->filterweek) $data->where('week',$this->filterweek);                        
+        if($this->filterproject) $data->where('project',$this->filterproject);                        
         
+        return view('livewire.team-schedule.data')->with(['data'=>$data->paginate(50)]);   
     }
-
-
 }
-
-
-
