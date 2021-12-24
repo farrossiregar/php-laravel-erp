1198ab076ad3baaf75ea07aca27ba097601fa4a8
farrossiregar <farros.jackson@gmail.com>
2021-12-25 01:57:30 +0700

    update asset req

--- app/Http/Livewire/AssetRequest/Add.php
+++ app/Http/Livewire/AssetRequest/Add.php
@@ -18,18 +18,49 @@ class Add extends Component
     protected $paginationTheme = 'bootstrap';
     
     use WithFileUploads;
-    public $dataproject, $company_name, $project, $client_project_id, $region, $employee_name;
+    public $dataproject, $company_name, $project, $client_project_id, $region, $employee_name, $position, $datalocation;
     public $asset_type, $asset_name, $location, $quantity, $dimension, $detail, $file, $reason_request, $link;
 
     public function render()
     {
 
         $user = \App\Models\Employee::where('user_id', Auth::user()->id)->first();
-        
+        // dd($user);
         $this->employee_name = $user->name;
-        $this->project = \App\Models\ClientProject::where('id', $user->project)->first()->name;
-        $this->region = \App\Models\Region::where('id', $user->region_id)->first()->region_code;
+        $this->position = get_position($user->user_access_id);
+        // $this->project = \App\Models\ClientProject::where('id', $user->project)->first()->name;
+        // $this->region = \App\Models\Region::where('id', $user->region_id)->first()->region_code;
 
+        $this->dataproject = \App\Models\ClientProject::orderBy('id', 'desc')
+                                ->where('company_id', Session::get('company_id'))
+                                ->where('is_project', '1')
+                                ->get();
+
+        $this->datalocation = \App\Models\Dophomebasemaster::where('status', '1')->orderBy('id', 'desc')->get();
+
+        if($this->project){
+            $getproject = \App\Models\ClientProject::where('id', $this->project)
+                                                    ->where('company_id', Session::get('company_id'))
+                                                    ->where('is_project', '1')
+                                                    ->first();
+
+            if($getproject){
+                if($getproject->region_id){
+                    $this->region = \App\Models\Region::where('id', $getproject->region_id)->first()->region_code;
+                }else{
+                    $this->region = '';
+                }
+            }else{
+                $this->region = '';
+            }
+
+            if($this->region){
+                $this->datalocation = \App\Models\Dophomebasemaster::where('project', $getproject->name)
+                                                                    ->where('region', $this->region)
+                                                                    ->where('status', '1')
+                                                                    ->orderBy('id', 'desc')->get();
+            }
+        }
         
         return view('livewire.asset-request.add');
     }
@@ -41,11 +72,12 @@ class Add extends Component
         $user                           = \App\Models\Employee::where('user_id', Auth::user()->id)->first();
         $data                           = new \App\Models\AssetRequest();
         $data->company_name             = Session::get('company_id');
-        $data->project                  = \App\Models\ClientProject::where('id', $user->project)->first()->name;
-        // $data->client_project_id        = \App\Models\ClientProject::where('name', $this->project)->first()->id;
+        $data->clien_project_id         = $this->project;
+        $data->project                  = \App\Models\ClientProject::where('name', $this->project)->first()->name;
         
-        $data->region                   = \App\Models\Region::where('id', $user->region_id)->first()->region_code;
-        $data->name                     = $user->name;
+        // $data->position                 = $this->position;
+        $data->region                   = $this->region;
+        $data->name                     = $this->employee_name;
         $data->asset_type               = $this->asset_type;
         $data->asset_name               = $this->asset_name;
         $data->location                 = $this->location;
