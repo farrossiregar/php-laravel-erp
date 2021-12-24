c960f726321562c31f985163a3f951ba40b9cd7d
Ramdoni <doni.engine@gmail.com>
2021-12-24 13:28:54 +0700

    update: drug test

---
+++ resources/views/livewire/drugtest/dashboard.blade.php
@@ -0,0 +1,118 @@
+<div>
+    <div class="row mb-3">
+        <div class="col-md-1" wire:ignore wire:model="filter_tahun">
+            <select class="form-control">
+                <option value="">-- Tahun --</option>
+                @foreach ($tahun as $item)
+                    @if(empty($item->tahun)) @continue @endif
+                    <option>{{$item->tahun}}</option>
+                @endforeach
+            </select>
+        </div>
+        <div>
+            <span wire:loading>
+                <i class="fa fa-spinner fa-pulse fa-2x fa-fw"></i>
+                <span class="sr-only">{{ __('Loading...') }}</span>
+            </span>
+        </div>
+    </div>
+    <div>
+        <canvas id="chart" style="height:400px;"></canvas>
+    </div>
+    <div class="table-responsive">
+        <table class="table m-b-0 c_list">
+            <thead>
+                <tr>
+                    <th class="text-center">Total Team</th>
+                    <th class="text-center">Done Drug Test</th>
+                    <th class="text-center">Not Yet Drug Test</th>
+                    <th class="text-center">Negative</th>
+                    <th class="text-center">Positive</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="text-center">{{$total_team}}</td>
+                    <td class="text-center">{{$done_drug_test}}</td>
+                    <td class="text-center">{{$not_yet_drug_test}}</td>
+                    <td class="text-center">{{$negative}}</td>
+                    <td class="text-center">{{$positive}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    @push('after-scripts')
+        <script src="{{ asset('assets/vendor/chartjs/Chart.bundle.min.js') }}?v=2"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0"></script>
+        <script>
+            var chart = "";
+            var labels = {!!$labels!!};
+            var series = {!!$series!!};
+            var data = {
+                labels: labels,
+                datasets: series
+            };
+
+            chart_(data);
+
+            Livewire.on('init-chart',(data)=>{
+                labels = JSON.parse(data.labels);
+                series = JSON.parse(data.series);
+                
+                var data = {
+                    labels: labels,
+                    datasets: series
+                };
+                chart_(data);
+            });
+
+            function chart_(data){
+                if(chart!=="") chart.destroy();
+                
+                const config = {
+                    type: 'horizontalBar',
+                    data: data,
+                    options: {
+                        maintainAspectRatio: false,
+                        indexAxis: 'y',
+                        elements: {
+                            bar: {
+                                borderWidth: 2,
+                            }
+                        },
+                        // responsive: true,
+                        legend: {
+                            position: 'top'
+                        },
+                        "hover": {
+                            "animationDuration": 0
+                        },
+                        "animation": {
+                            "duration": 1,
+                            "onComplete": function() {
+                                var chartInstance = this.chart,
+                                ctx = chartInstance.ctx;
+                
+                                ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                                ctx.textAlign = 'center';
+                                ctx.position = 'chartArea'
+
+                                this.data.datasets.forEach(function(dataset, i) {
+                                    var meta = chartInstance.controller.getDatasetMeta(i);
+                                    meta.data.forEach(function(bar, index) {
+                                        var data = dataset.data[index];
+                                        ctx.fillText(data, bar._model.x+10, bar._model.y);
+                                    });
+                                });
+                            }
+                        },
+                    }
+                };
+
+                var ctx = document.getElementById('chart').getContext('2d');
+                chart = new Chart(ctx, config);
+            }
+            
+        </script>
+    @endpush
+</div>
--- resources/views/livewire/drugtest/index.blade.php
+++ resources/views/livewire/drugtest/index.blade.php
@@ -3,128 +3,19 @@
 @section('title', __('Training Material'))
 <div class="row clearfix">
     <div class="col-lg-12">
+
         <div class="card">
-            <div class="row pt-3 pl-3">
-                <div class="col-md-2">
-                    <select class="form-control employee_id" wire:model="filter_employee_id">
-                        <option value=""> --- Employee --- </option>
-                        @foreach(\App\Models\Employee::where('is_use_android',1)->get() as $item)
-                        <option value="{{$item->id}}">{{$item->nik}} / {{$item->name}}</option>
-                        @endforeach
-                    </select>
+            <ul class="nav nav-tabs-new2">
+                <li class="nav-item"><a class="nav-link active show" data-toggle="tab" href="#dashboard">{{ __('Dashboard') }}</a></li>
+                <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#data">{{ __('Data') }}</a></li>
+            </ul>
+            <div class="tab-content">
+                <div class="tab-pane active show" id="dashboard">
+                    @livewire('drugtest.dashboard')
                 </div>
-                <div class="col-md-1 form-group">
-                    <input type="text" class="form-control date_created" placeholder="Date" />
+                <div class="tab-pane" id="data">
+                    @livewire('drugtest.data')
                 </div>
-                <div class="col-md-1" wire:ignore>
-                    <select class="form-control" wire:model="region_id" wire:change="$set('sub_region_id',null)">
-                        <option value=""> -- Region -- </option>
-                        @foreach($region as $item)
-                            <option value="{{$item->id}}">{{$item->region}}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="col-md-1">
-                    <select class="form-control" wire:model="sub_region_id">
-                        <option value=""> -- Sub Region -- </option>
-                        @foreach($sub_region as $item)
-                            <option value="{{$item->id}}">{{$item->name}}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="col-md-1">
-                    <select class="form-control" wire:model="filter_tahun">
-                        <option value=""> -- Tahun -- </option>
-                        @foreach(\App\Models\DrugTest::groupBy('tahun')->get() as $item)
-                            <option>{{$item->tahun}}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="col-md-1">
-                    <select class="form-control" wire:model="filter_batch">
-                        <option value=""> -- Batch -- </option>
-                        <option>1</option>
-                        <option>2</option>
-                    </select>
-                </div>
-                <div class="col-md-4">
-                    @if(check_access('drug-test.insert'))
-                        <a href="javascript:void(0)" class="btn btn-info" data-toggle="modal" data-target="#modal_add_drug_test"><i class="fa fa-plus"></i> Drug Test</a>
-                    @endif
-                    @if(check_access('drug-test.download'))
-                        <a href="javascript:void(0)" wire:click="downloadExcel" class="btn btn-success"><i class="fa fa-download"></i> Download</a>
-                    @endif
-                    <span wire:loading>
-                        <i class="fa fa-spinner fa-pulse fa-2x fa-fw"></i>
-                        <span class="sr-only">{{ __('Loading...') }}</span>
-                    </span>
-                </div>
-            </div>
-            <div class="body">
-                <div class="table-responsive">
-                    <table class="table table-hover m-b-0 c_list">
-                        <thead>
-                            <tr style="background:#eee;">
-                                <th style="width:50px;">No</th>         
-                                <th>Region</th>   
-                                <th>Sub Region</th>   
-                                <th>Employee</th>   
-                                <th>Tahun</th>
-                                <th class="text-center">Batch</th>
-                                <th>Remark</th>
-                                <th class="text-center">Status</th>
-                                <th class="text-center">File</th>
-                                <th>Date Submited</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($data as $k => $item)
-                                <tr>
-                                    <td>{{$k+1}}</td>
-                                    <td>{{isset($item->employee->region->region) ? $item->employee->region->region : ''}}</td>
-                                    <td>{{isset($item->employee->sub_region->name) ? $item->employee->sub_region->name : ''}}</td>
-                                    <td>
-                                        <!-- <a href="javascript:void(0)" class="text-danger" wire:click="delete({{$item->id}})"><i class="fa fa-trash"></i></a> -->
-                                        {{isset($item->employee->name) ? $item->employee->name : ''}}
-                                    </td>
-                                    <td>{{$item->tahun}}</td>
-                                    <td class="text-center">{{$item->batch}}</td>
-                                    <td>{{isset($item->remark) ? $item->remark : ''}}</td>
-                                    <td class="text-center">
-                                        @if($item->status_drug==0)
-                                            <span class="badge badge-warning">Not Submited</span>
-                                        @endif
-                                        @if($item->status_drug==1)
-                                            <span class="badge badge-danger">Positif</span>
-                                        @endif
-                                        @if($item->status_drug==2)
-                                            <span class="badge badge-success">Negatif</span>
-                                        @endif
-                                    </td>
-                                    <td class="text-center">
-                                        @foreach(\App\Models\DrugTestUpload::where('drug_test_id',$item->id)->get() as $img)
-                                            <a href="{{ $img->image }}" target="_blank"><i class="fa fa-file"></i></a>
-                                        @endforeach
-                                        @if($is_edit_image)
-                                            <a href="javascript:void(0)" class="ml-2 text-danger" wire:click="set_id({{$item->id}})" data-toggle="modal" data-target="#modal_edit_uploaded"><i class="fa fa-edit"></i></a>
-                                        @endif
-                                    </td>
-                                    <td>
-                                        @if($item->date_submited)
-                                            {{date('d-M-Y',strtotime($item->date_submited))}}
-                                        @endif
-                                    </td>
-                                </tr>
-                            @endforeach
-                            @if($data->count() ==0)
-                            <tr>
-                                <td colspan="5" class="text-center"><i>empty</i></td>
-                            </tr>
-                            @endif
-                        </tbody>
-                    </table>
-                </div>
-                {{$data->links()}}
             </div>
         </div>
     </div>
